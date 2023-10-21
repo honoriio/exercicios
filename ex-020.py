e91d77eb47ac9c7f2431469edb5ab709e6f1a51b
Diego Honório <83922000+honoriio@users.noreply.github.com>
2023-10-21 17:07:52 -0300

    add

--- ex-020.py
+++ ex-020.py
@@ -1,5 +1,9 @@
 import pygame
 
-pygame.mixer.init()
-pygame.mixer.music.load("ex020.mp3")
+pygame.init()
+pygame.mixer.music.load('ex020.mp3')
 pygame.mixer.music.play()
+pygame.event.wait()
+
+while pygame.mixer.music.get_busy():
+    pass
