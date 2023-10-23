795726f46e9f9462c0c84938e46345eb0162bb66
Diego Honório <83922000+honoriio@users.noreply.github.com>
2023-10-23 15:47:59 -0300

    add

--- ex-021.py
+++ ex-021.py
@@ -1,4 +1,17 @@
+nome = input('Qual é o seu nome completo? : ')
 
-frase = str('curso em video python')
+print(nome.upper())
+print(nome.lower())
 
-print(frase[3:15:2])
+
+contagem = 0
+for caracter in nome:
+    if caracter != " ":
+        contagem += 1
+print(f'O total de caracteres sem espaços é: {contagem}')
+
+
+palavras = nome.split()
+primeiro_nome = palavras[0]
+comprimento = len(primeiro_nome)
+print(f'O primeiro nome tem {comprimento} letras')
