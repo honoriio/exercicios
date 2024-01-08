7bf6e9f7b44bb62a1649e82177451827a0e8ac59
Diego Honório <diegohonoriiio@gmail.com>
2024-01-08 12:55:42 -0300

    new exercit

---
+++ ex-95.py
@@ -0,0 +1,14 @@
+def escreva(txt):
+    tam = len(texto)
+    print('-' * tam)
+    print(txt)
+    print('-' * tam)
+    
+
+
+print('=' * 52)
+print('FORMATAR TEXTO'.center(52))
+print('=' * 52)
+
+texto = input('Digite uma frase: ')
+escreva(texto)
